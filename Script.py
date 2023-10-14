aa3e7a723b1f10e6166d8c7f29c726515f40a76a
sachinkpt <145140896+sachinkpt@users.noreply.github.com>
2023-10-14 23:02:08 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -160,7 +160,7 @@ Nᴀᴍᴇ - {}
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
     CAPTION = """
-<b> {file_name}</b>"""
+<b> {file_caption}</b>"""
 
     IMDB_TEMPLATE_TXT = """
 <b>𝐇𝐞𝐲 {message.from_user.mention}, 𝐇𝐞𝐫𝐞 𝐢𝐬 𝐭𝐡𝐞 𝐫𝐞𝐬𝐮𝐥𝐭 𝐟𝐨𝐫 𝐲𝐨𝐮𝐫 {query}
