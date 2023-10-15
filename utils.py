9984b365372e09cc580e5573ae11309ee86ed5f7
PrimeX <103210935+DevPrimeX@users.noreply.github.com>
2023-10-15 09:42:49 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -655,7 +655,7 @@ async def send_all(bot, userid, files, ident):
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                        InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
+                  #      InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                         InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
            #         ],[
            #             InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Omni_Botz")
